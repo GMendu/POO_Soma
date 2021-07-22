71ebdff8d60764e33700ded8bdf43897c8e624d7
Aluno <Aluno@LAB2-04>
2021-07-22 14:10:44 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1-24v09/Numero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula1_24v09
+{
+    class Numero
+    {
+        public double Valor { get; set; }
+        public double Valor2 { get; set; }
+    }
+}
---
+++ Aula1-24v09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula1_24v09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numero numero = new Numero();
+            Console.Write("Digite um valor: ");
+            numero.Valor = double.Parse(Console.ReadLine());
+            Console.Write("Digite outro valor: ");
+            numero.Valor2 = double.Parse(Console.ReadLine());
+            double soma = numero.Valor + numero.Valor2;
+            Console.WriteLine("A soma é: " + soma);
+            Console.ReadKey();
+        }
+    }
+}
